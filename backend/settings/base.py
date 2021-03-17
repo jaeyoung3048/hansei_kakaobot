b224c72b4e2d4e4576e4833053b39f3bee5c63be
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-03-17 10:52:39 +0900

    FIX: Product build settings fix

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from utils.secrets import get_secret
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,4 +124,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
